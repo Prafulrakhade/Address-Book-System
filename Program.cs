b66c16d6c47714cd1aced32cd35487b02e9b470c
Praful <prafulrakhade02@gmail.com>
2022-03-20 15:23:27 +0530

    [praful] added:

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------- WELCOME TO THE ADDRESS BOOK PROGRAM -----------------");
+            Console.WriteLine();
+        }
+    }
+}
